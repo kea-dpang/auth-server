0458e3df926c6e0c988295e51c67906c219093c6
SeungHyeon <namsh1125@naver.com>
2024-01-16 17:20:12 +0900

    Feat: 인증 및 사용자 서비스 인터페이스 추가

---
+++ src/main/kotlin/kea/dpang/auth/service/TokenService.kt
@@ -0,0 +1,29 @@
+package kea.dpang.auth.service
+
+import kea.dpang.auth.dto.Token
+
+interface TokenService {
+
+    /**
+     * 토큰 생성 메서드.
+     *
+     * 사용자 식별자를 기반으로 새로운 인증 토큰을 반환합니다.
+     */
+    fun createToken(identifier: String): Token
+
+    /**
+     * 토큰 갱신 메서드.
+     *
+     * 사용자의 기존 리프레시 토큰을 입력받아 새로운 인증 토큰을 반환합니다.
+     */
+    fun refreshToken(token: String): Token
+
+    /**
+     * 토큰 제거 메서드.
+     *
+     * 사용자의 리프레시 토큰을 입력받아 해당 토큰을 완전히 제거합니다.
+     * 이 메서드는 로그아웃 및 회원 탈퇴 시 사용됩니다.
+     */
+    fun removeToken(token: String)
+
+}
