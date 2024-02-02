684fa6a262c097230a612d349ab23b28e7991ccf
SeungHyeon <namsh1125@naver.com>
2024-02-02 11:48:06 +0900

    Update: 예외 메시지 한국어로 변경

--- src/main/kotlin/kea/dpang/auth/exception/InvalidRefreshTokenException.kt
+++ src/main/kotlin/kea/dpang/auth/exception/InvalidRefreshTokenException.kt
@@ -1,3 +1,3 @@
 package kea.dpang.auth.exception
 
-class InvalidRefreshTokenException(token: String) : RuntimeException("Invalid refresh token: $token")
+class InvalidRefreshTokenException(token: String) : RuntimeException("유효하지 않은 리프레시 토큰: $token")
