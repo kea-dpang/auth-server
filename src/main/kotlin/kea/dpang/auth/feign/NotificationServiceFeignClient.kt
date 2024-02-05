f89bcdd382302a13aaefa102e5fddc51e4d48d7d
SeungHyeon <namsh1125@naver.com>
2024-02-06 01:36:42 +0900

    !HOTFIX: FeignClient의 메서드에 직접 헤더 설정

--- src/main/kotlin/kea/dpang/auth/feign/NotificationServiceFeignClient.kt
+++ src/main/kotlin/kea/dpang/auth/feign/NotificationServiceFeignClient.kt
@@ -1,6 +1,5 @@
 package kea.dpang.auth.feign
 
-import feign.Headers
 import kea.dpang.auth.base.BaseResponse
 import kea.dpang.auth.feign.dto.EmailNotificationRequestDto
 import org.springframework.cloud.openfeign.FeignClient
@@ -9,11 +8,9 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 
 
-@Headers("X-DPANG-CLIENT-ROLE: SYSTEM")
 @FeignClient(name = "notification-server")
 fun interface NotificationServiceFeignClient {
 
-    @Headers("X-DPANG-CLIENT-ROLE: SYSTEM")
-    @PostMapping("/api/notifications/email")
+    @PostMapping("/api/notifications/email", headers = ["X-DPANG-CLIENT-ROLE: SYSTEM"])
     fun sendEmailVerificationCode(@RequestBody dto: EmailNotificationRequestDto) : ResponseEntity<BaseResponse>
 }
