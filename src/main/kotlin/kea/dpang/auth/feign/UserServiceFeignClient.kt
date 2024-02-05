ba66f34793117a532ba02c4d20e00c0172e810ed
SeungHyeon <namsh1125@naver.com>
2024-02-06 02:44:41 +0900

    Update: 로그인시 사용자 이름을 추가로 반환하도록 구현

--- src/main/kotlin/kea/dpang/auth/feign/UserServiceFeignClient.kt
+++ src/main/kotlin/kea/dpang/auth/feign/UserServiceFeignClient.kt
@@ -1,20 +1,22 @@
 package kea.dpang.auth.feign
 
 import kea.dpang.auth.base.BaseResponse
+import kea.dpang.auth.base.SuccessResponse
 import kea.dpang.auth.feign.dto.DeleteUserRequestDto
 import kea.dpang.auth.feign.dto.RegisterUserRequestDto
+import kea.dpang.auth.feign.dto.UserDto
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.*
 
 @FeignClient(name = "user-server")
 interface UserServiceFeignClient {
 
     @PostMapping("/api/users/register")
     fun registerUser(@RequestBody dto: RegisterUserRequestDto): ResponseEntity<BaseResponse>
+
+    @GetMapping("/api/users/{userId}")
+    fun getUserInfo(@PathVariable userId: Long): ResponseEntity<SuccessResponse<UserDto>>
 
     @DeleteMapping("/api/users/{userId}")
     fun deleteUserInfo(@PathVariable userId: Long, @RequestBody dto: DeleteUserRequestDto): ResponseEntity<BaseResponse>
