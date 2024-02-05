9a5e99494886d380be2fc2432da9d36d6e86b288
SeungHyeon <namsh1125@naver.com>
2024-02-06 01:51:59 +0900

    !HOTFIX: FeignConfig 설정 클래스 추가 및 RequestInterceptor 빈 등록

--- src/main/kotlin/kea/dpang/auth/feign/NotificationServiceFeignClient.kt
+++ src/main/kotlin/kea/dpang/auth/feign/NotificationServiceFeignClient.kt
@@ -1,6 +1,7 @@
 package kea.dpang.auth.feign
 
 import kea.dpang.auth.base.BaseResponse
+import kea.dpang.auth.config.FeignConfig
 import kea.dpang.auth.feign.dto.EmailNotificationRequestDto
 import org.springframework.cloud.openfeign.FeignClient
 import org.springframework.http.ResponseEntity
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 
 
-@FeignClient(name = "notification-server")
+@FeignClient(name = "notification-server", configuration = [FeignConfig::class])
 fun interface NotificationServiceFeignClient {
 
     @PostMapping("/api/notifications/email", headers = ["X-DPANG-CLIENT-ROLE: SYSTEM"])
